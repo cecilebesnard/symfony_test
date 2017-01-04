ece036f05165b1bc8485b125effce50c55908225
Your Name <you@example.com>
2017-01-04 12:03:08 +0100

    save1

---
+++ src/adminBundle/Controller/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace adminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ProductController extends Controller
+{
+    /**
+     * @Route("/admin/product" , name="product")
+     */
+    public function productAction()
+    {
+        $products = [
+            [
+                "id" => 1,
+                "title" => "Mon premier produit",
+                "description" => "lorem ipsum",
+                "date_created" => new \DateTime('now'),
+                "prix" => 10
+            ],
+            [
+                "id" => 2,
+                "title" => "Mon deuxième produit",
+                "description" => "lorem ipsum",
+                "date_created" => new \DateTime('now'),
+                "prix" => 20
+            ],
+            [
+                "id" => 3,
+                "title" => "Mon troisième produit",
+                "description" => "lorem ipsum",
+                "date_created" => new \DateTime('now'),
+                "prix" => 30
+            ],
+            [
+                "id" => 4,
+                "title" => "",
+                "description" => "lorem ipsum",
+                "date_created" => new \DateTime('now'),
+                "prix" => 410
+            ],
+        ];
+
+        return $this->render('Product/product.html.twig' ,[ 'products' => $products ]);
+    }
+
+
+}
